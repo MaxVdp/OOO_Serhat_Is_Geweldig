3a55159de660e073b1a9dfa85932804e3832b54e
BaeyensRobbe <robbe.baeyens@student.ucll.be>
2022-09-19 12:29:22 +0200

    package domain

--- Labo1/src/UI/Main.java
+++ Labo1/src/UI/Main.java
@@ -1,5 +1,27 @@
 package UI;
 
-public class Main {
+import domain.Shop;
 
+import javax.swing.*;
+
+import static domain.Shop.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Shop shop = new Shop();
+
+        String menu = "1. Add product\n2. Show product\n3. Show rental price\n\n0. Quit";
+        int choice = -1;
+        while (choice != 0) {
+            String choiceString = JOptionPane.showInputDialog(menu);
+            choice = Integer.parseInt(choiceString);
+            if (choice == 1) {
+                addProduct(shop);
+            } else if (choice == 2) {
+                showProduct(shop);
+            } else if (choice == 3){
+                showPrice(shop);
+            }
+        }
+    }
 }
