0cb79f4245ca7c914d7030c4b0687f2d19296a26
MaxVdp <max.vandeput@student.ucll.be>
2022-09-26 12:12:43 +0200

    Update MirroringBehaviour.java

--- Labo2/src/domain/MirroringBehaviour.java
+++ Labo2/src/domain/MirroringBehaviour.java
@@ -1,4 +1,13 @@
 package domain;
 
 public class MirroringBehaviour {
+    private String tekst;
+
+    private String encode(String tekst){
+        String newString = "";
+        for(String l : tekst.split("")){
+            newString = l + newString;
+        }
+        return newString;
+    }
 }
