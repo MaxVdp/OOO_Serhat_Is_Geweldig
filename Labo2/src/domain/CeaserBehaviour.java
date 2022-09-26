358b0c0e63d7672c1262b86cbf2606e4b4e19649
TimoDeGrande <timo.degrande@student.ucll.be>
2022-09-26 12:12:29 +0200

    Update CeaserBehaviour.java

--- Labo2/src/domain/CeaserBehaviour.java
+++ Labo2/src/domain/CeaserBehaviour.java
@@ -1,4 +1,19 @@
 package domain;
 
-public class CeaserBehaviour {
+public class CeaserBehaviour implements EncodeBehaviour{
+    private String tekst;
+    private int defaultVerplaatsing = 5;
+
+    public String encode(String tekst, int verplaatsing){
+
+    }
+    public String decode(String tekst, int verplaatsing){
+
+    }
+    public String encode(String tekst){
+
+    }
+    public String decode(String tekst){
+
+    }
 }
