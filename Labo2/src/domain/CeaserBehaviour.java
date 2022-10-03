c8ca3da4e05a9a920554e115be8da3bdd8ab9f6f
TimoDeGrande <timo.degrande@student.ucll.be>
2022-10-03 10:56:10 +0200

    sdf

--- Labo2/src/domain/CeaserBehaviour.java
+++ Labo2/src/domain/CeaserBehaviour.java
@@ -6,7 +6,6 @@ public class CeaserBehaviour implements EncodeBehaviour{
     static String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
     public String encode(String tekst, int verplaatsing){
-<<<<<<< HEAD
         String result = "";
         for(int j = 0; j < tekst.length(); j++){
             for(int i = 0; i<=25; i++){
@@ -24,9 +23,6 @@ public class CeaserBehaviour implements EncodeBehaviour{
             }
         }
         return result;
-=======
-        return null;
->>>>>>> 8943b5e9f90ccc9cdd1b374f8d7dfe966e3b2f5b
     }
     public String decode(String tekst, int verplaatsing){
         return null;
