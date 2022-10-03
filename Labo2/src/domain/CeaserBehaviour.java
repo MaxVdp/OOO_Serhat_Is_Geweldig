8dad7bf9198c0678916dae28224c90ba689b98bf
Tim-Lefever <tim.lefever@student.ucll.be>
2022-10-03 11:48:41 +0200

    Update CeaserBehaviour.java

--- Labo2/src/domain/CeaserBehaviour.java
+++ Labo2/src/domain/CeaserBehaviour.java
@@ -3,28 +3,6 @@ package domain;
 import java.util.Arrays;
 import java.util.List;
 
-public class CeaserBehaviour implements EncodeBehaviour{
-    private String tekst;
-    private int defaultVerplaatsing = 5;
-    static String[] ap = ("abcdefghijklmnopqrstuvwxyz").split("");
-    static List<String> alphabet = Arrays.asList(ap);
-
-    public String encode(String tekst){
-        String[] letters = tekst.split("");
-        String result = "";
-        for(String letter: letters){
-            if(letter.equals(" ")){
-                result += " ";
-            }
-            else{
-                int index = alphabet.indexOf(letter.toLowerCase());
-                if(index + defaultVerplaatsing > 25){
-                    index -= 25 + defaultVerplaatsing;
-                    result += alphabet.get(index);
-                }
-                else{
-                    index += defaultVerplaatsing;
-                    result += alphabet.get(index);
 public class CeaserBehaviour implements EncodeBehaviour {
     private int defaultVerplaatsing = 5;
     private static String alphabet = "abcdefghijklmnopqrstuvwxyz";
