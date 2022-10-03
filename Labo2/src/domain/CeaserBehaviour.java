014a0f6bc306e60984c8a9b988516af78b866956
Tim-Lefever <tim.lefever@student.ucll.be>
2022-10-03 11:55:01 +0200

    static param

--- Labo2/src/domain/CeaserBehaviour.java
+++ Labo2/src/domain/CeaserBehaviour.java
@@ -5,23 +5,22 @@ import java.util.List;
 
 public class CeaserBehaviour implements EncodeBehaviour {
     private int defaultVerplaatsing = 5;
-    private static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    private static String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 
     public String decode(String tekst, int verplaatsing) {
         return this.encode(tekst, -verplaatsing);
     }
 
     public String encode(String tekst, int verplaatsing) {
-        String alfabet = "abcdefghijklmnopqrstuvwxyz";
         String newString = "";
         for (String c : tekst.split("")) {
             if (c.equals(" ")) {
                 newString += c;
             } else {
-                int index = alfabet.indexOf(c.toLowerCase());
+                int index = ALPHABET.indexOf(c.toLowerCase());
                 int newIndex = index + verplaatsing;
                 newIndex = ((newIndex % 26) + 26) % 26;
-                String newChar = alfabet.substring(newIndex, newIndex + 1);
+                String newChar = ALPHABET.substring(newIndex, newIndex + 1);
                 if (c.equals(c.toLowerCase())) {
                     newString += newChar;
                 } else {
