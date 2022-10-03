7c67592fe44cc06d5376ce20a9a2563b08808d90
Tim-Lefever <tim.lefever@student.ucll.be>
2022-10-03 11:53:09 +0200

    deleted errors

--- Labo2/src/domain/CeaserBehaviour.java
+++ Labo2/src/domain/CeaserBehaviour.java
@@ -7,56 +7,11 @@ public class CeaserBehaviour implements EncodeBehaviour {
     private int defaultVerplaatsing = 5;
     private static String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+    public String decode(String tekst, int verplaatsing) {
+        return this.encode(tekst, -verplaatsing);
+    }
 
     public String encode(String tekst, int verplaatsing) {
-        String result = "";
-        for (int j = 0; j < tekst.length(); j++) {
-            for (int i = 0; i <= 25; i++) {
-                if (tekst.charAt(j).equals(" ")) {
-                    result += " ";
-                } else if (tekst.charAt(j) == (alphabet.charAt(i))) {
-                    if (i + verplaatsing > 25) {
-                        result += alphabet.charAt(i + verplaatsing - 25);
-                    } else {
-                        result += alphabet.charAt(i + verplaatsing);
-                    }
-                }
-            }
-        }
-        return result;
-    }
-    public String encode(String tekst, int verplaatsing){
-        String[] letters = tekst.split("");
-        String result = "";
-        for(String letter: letters){
-            if(letter.equals(" ")){
-                result += " ";
-            }
-            else{
-                int index = alphabet.indexOf(letter.toLowerCase());
-                if(index + verplaatsing > 25){
-                    index -= 25 + verplaatsing;
-                    result += alphabet.get(index);
-                }
-                else if(index + verplaatsing < 0){
-                    index += 25 + verplaatsing;
-                    result += alphabet.get(index);
-                }
-                else{
-                    index += verplaatsing;
-                    result += alphabet.get(index);
-                }
-            }
-        }
-        return result;
-    }
-    public String decode(String tekst){
-        return this.encode(tekst, -defaultVerplaatsing);
-    }
-    public String decode(String tekst, int verplaatsing){
-        return this.encode(tekst, -verplaatsing);
-
-    public String encode3(String tekst, int verplaatsing) {
         String alfabet = "abcdefghijklmnopqrstuvwxyz";
         String newString = "";
         for (String c : tekst.split("")) {
@@ -77,7 +32,13 @@ public class CeaserBehaviour implements EncodeBehaviour {
         return newString;
     }
 
-    public String encode(String tekst) {
-        return this.encode(tekst, this.defaultVerplaatsing);
+    @Override
+    public String encode(String text) {
+        return this.encode(text, defaultVerplaatsing);
+    }
+
+    @Override
+    public String decode(String text) {
+        return this.decode(text, -defaultVerplaatsing);
     }
 }
