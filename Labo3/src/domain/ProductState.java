42bc9e5a7a075a4a4e01b171ce97fd3d8847b130
Tim-Lefever <tim.lefever@student.ucll.be>
2022-10-10 12:31:17 +0200

    formatting

--- Labo3/src/domain/ProductState.java
+++ Labo3/src/domain/ProductState.java
@@ -2,26 +2,14 @@ package domain;
 
 public interface ProductState {
 
-    default void rent() {
-        throw new IllegalArgumentException("Cannot rent item.");
-    }
+    default void rent() { throw new IllegalArgumentException("Cannot rent item."); }
 
-    default void damage() {
-        throw new IllegalArgumentException("Cannot damage item.");
-    }
+    default void damage() { throw new IllegalArgumentException("Cannot damage item."); }
 
-    default void delete() {
-        throw new IllegalArgumentException("Cannot delete item.");
+    default void delete() { throw new IllegalArgumentException("Cannot delete item."); }
 
-    }
+    default void repair() { throw new IllegalArgumentException("Cannot repair item."); }
 
-    default void repair() {
-        throw new IllegalArgumentException("Cannot repair item.");
-
-    }
-
-    default void bringBack() {
-        throw new IllegalArgumentException("Cannot bring item back.");
-    }
+    default void bringBack() { throw new IllegalArgumentException("Cannot bring item back."); }
 
 }
