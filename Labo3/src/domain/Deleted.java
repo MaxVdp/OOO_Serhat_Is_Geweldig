e375aca0633cc7cfe2e5d7e9d94ce380d91d50cb
Tim-Lefever <tim.lefever@student.ucll.be>
2022-10-12 23:18:57 +0200

    Deleted updated

--- Labo3/src/domain/Deleted.java
+++ Labo3/src/domain/Deleted.java
@@ -2,4 +2,9 @@ package domain;
 
 public class Deleted implements ProductState {
 
+    //ik heb geen idee wat er in deze klasse moet staan, hij mag volledig leeg zijn maar hij moet wel gewoon bestaan
+    //zodat er geen error gegooid wordt.
+    public Deleted() {
+        System.out.println("Forget me I'm deleted!!!!");
+    }
 }
