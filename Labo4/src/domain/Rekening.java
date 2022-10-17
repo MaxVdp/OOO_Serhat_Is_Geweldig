7e660cfdf77342ada4ddf400cc52a42b4b3d01e0
Tim-Lefever <tim.lefever@student.ucll.be>
2022-10-17 11:43:31 +0200

    some more classes

--- Labo4/src/domain/Rekening.java
+++ Labo4/src/domain/Rekening.java
@@ -1,4 +1,27 @@
 package domain;
 
 public class Rekening {
+    private String rekeningnummer;
+    private double saldo;
+
+    public Rekening(String rekeningnummer, double saldo) {
+        this.setRekeningnummer(rekeningnummer);
+        this.setSaldo(saldo);
+    }
+
+    public String getRekeningnummer() {
+        return rekeningnummer;
+    }
+
+    public void setRekeningnummer(String rekeningnummer) {
+        this.rekeningnummer = rekeningnummer;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 }
